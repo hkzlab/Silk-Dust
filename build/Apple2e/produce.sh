8e2cfcb3a0a1f426e2aaadc51535d04824dd2e5a
Fabio Battaglia <tabaglio@posteo.net>
2026-01-17 09:53:29 +0100

    Add splash screen

--- build/Apple2e/produce.sh
+++ build/Apple2e/produce.sh
@@ -5,14 +5,14 @@
 # platforms are defined in a single config file in the parent directory:
 . ../config.sh
 
-# Assemble the disk image for the english version of the game
+# Assemble the disk image
 cp dsk/prodos.po ./silkdust_en.po
-java -jar $acjarfile -as ./silkdust_en.po SDUST.SYSTEM < silkdust_en.bin
+java -jar $acjarfile -as ./silkdust_en.po STARTUP < splash.bin
+java -jar $acjarfile -as ./silkdust_en.po GAME < silkdust_en.bin
+java -jar $acjarfile -p ./silkdust_en.po SPLASH.HGR 0 < dsk/splash.hgr
 java -jar $acjarfile -p ./silkdust_en.po EM.DRV 0 < dsk/a2e.auxmem.emd
 java -jar $acjarfile -p ./silkdust_en.po TEXT.DAT 0 < text_en.dat
 
-cp ../readme.txt .
-
-rm *.zip
-zip -r  *.po readme.txt 
-cp AppleIIe_SilkDust.zip $ditdir
+rm  AppleIIe_SilkDust_EN.zip
+zip -r AppleIIe_SilkDust_EN.zip silkdust_en.po
+cp AppleIIe_SilkDust_EN.zip $ditdir
